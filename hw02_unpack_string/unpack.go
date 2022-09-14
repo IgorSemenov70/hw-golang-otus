b97ae2878b1e45747b6e765d9a3ef15ea499edab
IgorSemenov70 <87079215+IgorSemenov70@users.noreply.github.com>
2022-09-14 14:10:38 +0700

    hw2 complete

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,71 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func checkFirstIsDigit(packedString string) bool {
+	// true если первый элемент число
+	return packedString != "" && unicode.IsDigit(rune(packedString[0]))
+}
+
+func checkCurrentIsDigitAndPreviousNotBackslash(item rune, index int, sr []rune) bool {
+	// true если текущий элемент число, предыдущий элемент число и пред предыдущий
+	return unicode.IsDigit(item) && unicode.IsDigit(sr[index-1]) && sr[index-2] != '\\'
+}
+
+func checkBackslash(item rune, backslash bool) bool {
+	// true если элемент равен \\
+	return item == '\\' && !backslash
+}
+
+func checkBackslashAndIsLetter(backslash bool, item rune) bool {
+	// true если предущий элемент был // и текущий элемент буква
+	return backslash && unicode.IsLetter(item)
+}
+
+func Unpack(s string) (string, error) {
+	sr := []rune(s)
+	builder := strings.Builder{}
+	var n int
+	var backslash bool
+
+	if checkFirstIsDigit(s) {
+		return "", ErrInvalidString
+	}
+
+	for index, item := range sr {
+		if checkCurrentIsDigitAndPreviousNotBackslash(item, index, sr) {
+			return "", ErrInvalidString
+		}
+		if checkBackslash(item, backslash) {
+			backslash = true
+			continue
+		}
+		if checkBackslashAndIsLetter(backslash, item) {
+			return "", ErrInvalidString
+		}
+		if backslash {
+			builder.WriteString(string(item))
+			backslash = false
+			continue
+		}
+		if unicode.IsDigit(item) {
+			n = int(item - '0')
+			if n == 0 {
+				unpackedString := builder.String()
+				builder.Reset()
+				builder.WriteString(unpackedString[:len(unpackedString)-1])
+				continue
+			}
+			builder.WriteString(strings.Repeat(string(sr[index-1]), n-1))
+			continue
+		}
+		builder.WriteString(string(item))
+	}
+
+	return builder.String(), nil
 }
